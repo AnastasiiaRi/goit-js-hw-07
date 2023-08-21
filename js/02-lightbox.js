0535a0cd13859ed1a51f2420c915d6a039dfe0d7
AnastasiiaRi <archi.bocharova@gmail.com>
2023-08-21 21:12:55 +0300

    gallery-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const image = galleryItems
+.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+})
+.join("");
+gallery.insertAdjacentHTML("afterbegin", image);
+
+gallery.addEventListener("click", e => {
+    e.preventDefault();
+    if (e.target.nodeName !== "IMG") {
+          return;
+      } 
+  });
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
